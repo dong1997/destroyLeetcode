946b3e31f2a598a856884e6435519ef7f7700f73
dongcl <dongchenglong97@gmail.com>
2022-03-30 10:13:52 +0800

    lc215

--- Sort/lc215.cpp
+++ Sort/lc215.cpp
@@ -23,6 +23,7 @@ using namespace std;
 /*
  * maintain a small top heap of size K
  * after loop, the top element in small top heap is actually the Kth largest element in the array
+ *
  */
 int findKthLargest(vector<int> &nums, int k) {
     priority_queue<int, vector<int>, greater<>> pq;
